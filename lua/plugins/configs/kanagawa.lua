2aa179ea3a520dce4fe00e14574069a4520acc33
Simone Fidanza <fsimone2002@proton.me>
2023-06-25 19:13:15 +0200

    feat(colorscheme): override hi groups for barbar, remove bg_gutter

--- lua/plugins/configs/kanagawa.lua
+++ lua/plugins/configs/kanagawa.lua
@@ -5,21 +5,43 @@ local options = {
   overrides = function(colors)
     local theme = colors.theme
     return {
-      NormalFloat = { bg = "none" },
-      FloatBorder = { bg = "none" },
-      FloatTitle = { bg = "none" },
+      NormalFloat = { bg = "NONE" },
+      FloatBorder = { bg = "NONE" },
+      FloatTitle = { bg = "NONE" },
 
       -- Save an hlgroup with dark background and dimmed foreground
       -- so that you can use it where your still want darker windows.
       -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
       NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
 
+      BufferInactive = { fg = theme.ui.fg_dim, bg = theme.ui.bg_p1 },
+      BufferInactiveSign = { fg = theme.ui.bg, bg = theme.ui.bg_p1 },
+
+      BufferCurrent = { fg = theme.ui.fg, bg = theme.ui.bg_m2 },
+      BufferCurrentSign = { fg = theme.ui.bg, bg = theme.ui.bg_m2 },
+      -- BufferSign = { bg = theme.ui.bg_m3 },
+
       -- Popular plugins that open floats will link to NormalFloat by default;
       -- set their background accordingly if you wish to keep them dark and borderless
       -- LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
       -- MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+      -- More uniform colors for the popup menu.
+      Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+      PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+      PmenuSbar = { bg = theme.ui.bg_m1 },
+      PmenuThumb = { bg = theme.ui.bg_p2 },
     }
   end,
+  colors = {
+    theme = {
+      all = {
+        ui = {
+          bg_gutter = "NONE",
+        },
+      },
+    },
+  },
   theme = "dragon",
 }
 
