8257327ec9a5dd4442900de45909521fcfb3b7fa
Simone Fidanza <fsimone2002@proton.me>
2023-06-28 16:01:31 +0200

    feat: implement function to load mappings

--- lua/utils/fn.lua
+++ lua/utils/fn.lua
@@ -6,7 +6,6 @@
 local fn = {
   ---@type function Align a markdown table using `:Tabularize`
   align_table = function()
-    ---@type string Pattern to identify a markdown table
     local pattern = "^%s*|%s.*%s|%s*$"
     local linenr, colnr = vim.fn.line ".", vim.fn.col "."
     local curr_line = vim.fn.getline "."
@@ -25,6 +24,44 @@ local fn = {
       vim.cmd "normal! 0"
       vim.fn.search(("[^|]*|"):rep(col) .. ("\\s\\{-\\}"):rep(pos), "ce", linenr)
     end
+  end,
+
+  load_mappings = function(section, mapping_opt)
+    vim.schedule(function()
+      local tbl_merge = vim.tbl_deep_extend
+
+      local function set_section_map(section_values)
+        if section_values.plugin then
+          return
+        end
+
+        section_values.plugin = nil
+
+        for mode, mode_values in pairs(section_values) do
+          local default_opts = tbl_merge("force", { mode = mode }, mapping_opt or {})
+          for keybind, mapping_info in pairs(mode_values) do
+            -- merge default + user opts
+            local opts = tbl_merge("force", default_opts, mapping_info.opts or {})
+
+            mapping_info.opts, opts.mode = nil, nil
+            opts.desc = mapping_info[2]
+
+            vim.keymap.set(mode, keybind, mapping_info[1], opts)
+          end
+        end
+      end
+
+      local mappings = require "mappings"
+
+      if type(section) == "string" then
+        mappings[section]["plugin"] = nil
+        mappings = { mappings[section] }
+      end
+
+      for _, sect in pairs(mappings) do
+        set_section_map(sect)
+      end
+    end)
   end,
 }
 
