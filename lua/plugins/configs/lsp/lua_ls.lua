fe8ebbfffd63daa7c2f45343b80446f58be12bca
Simone Fidanza <fsimone2002@proton.me>
2023-06-28 16:12:40 +0200

    feat(lsp): add workspace library files

--- lua/plugins/configs/lsp/lua_ls.lua
+++ lua/plugins/configs/lsp/lua_ls.lua
@@ -20,6 +20,8 @@ return {
       vim.api.nvim_get_runtime_file("", true),
       [vim.fn.expand "$VIMRUNTIME/lua"] = true,
       [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+      [vim.fn.stdpath "data" .. "/lazy/extensions/nvchad_types"] = true,
+      [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
     },
 
     -- adjust these two values if your performance is not optimal
