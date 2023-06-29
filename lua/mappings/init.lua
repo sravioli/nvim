3c09da16142d5725d828adcea15deb503e818ed8
Simone Fidanza <fsimone2002@proton.me>
2023-06-29 15:32:07 +0200

    fix(keymaps): add todo-comments mappings

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -5,4 +5,5 @@ return {
   treesitter = require "mappings.tree-sitter",
   neotree = require "mappings.neotree",
   gitsigns = require "mappings.gitsigns",
+  todo_comments = require "mappings.todo-comments",
 }
