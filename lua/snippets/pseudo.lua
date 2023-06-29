d2d0e87f393018dfff23e5dfb17dc10a1d418e9a
Simone Fidanza <fsimone2002@proton.me>
2023-06-29 15:31:35 +0200

    fix(snippets): remove LuaSnip import and nodes

--- lua/snippets/pseudo.lua
+++ lua/snippets/pseudo.lua
@@ -1,33 +1,4 @@
-local present, ls = pcall(require, "luasnip")
-if not present then
-  return
-end
-
-local s = ls.snippet
--- local sn = ls.snippet_node
--- local isn = ls.indent_snippet_node
-local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
--- local c = ls.choice_node
--- local d = ls.dynamic_node
--- local r = ls.restore_node
--- local events = require "luasnip.util.events"
--- local ai = require "luasnip.nodes.absolute_indexer"
-local extras = require "luasnip.extras"
-local l = extras.lambda
--- local rep = extras.rep
--- local p = extras.partial
--- local m = extras.match
--- local n = extras.nonempty
--- local dl = extras.dynamic_lambda
-local fmt = require("luasnip.extras.fmt").fmt
--- local fmta = require("luasnip.extras.fmt").fmta
--- local conds = require "luasnip.extras.expand_conditions"
-local postfix = require("luasnip.extras.postfix").postfix
--- local types = require "luasnip.util.types"
--- local parse = require("luasnip.util.parser").parse_snippet
--- local ms = ls.multi_snippet
+---@diagnostic disable: undefined-global
 
 local function array_format(str, elem)
   local array, pos = str:match "^([%a_]+)%[([%w_]+)%]$"
@@ -35,7 +6,7 @@ local function array_format(str, elem)
     return str
   end
 
-  return string.format("%s in posizione %s di %s", elem, pos, array)
+  return string.format("%s in pos. %s di %s", elem, pos, array)
 end
 
 return {
--- lua/snippets/tex.lua
+++ lua/snippets/tex.lua
@@ -1,34 +1,4 @@
-local present, ls = pcall(require, "luasnip")
-if not present then
-  return
-end
-
-local s = ls.snippet
--- local sn = ls.snippet_node
--- local isn = ls.indent_snippet_node
-local t = ls.text_node
-local i = ls.insert_node
--- local f = ls.function_node
--- local c = ls.choice_node
--- local d = ls.dynamic_node
--- local r = ls.restore_node
--- local events = require "luasnip.util.events"
--- local ai = require "luasnip.nodes.absolute_indexer"
-local extras = require "luasnip.extras"
-local l = extras.lambda
--- local rep = extras.rep
--- local p = extras.partial
--- local m = extras.match
--- local n = extras.nonempty
--- local dl = extras.dynamic_lambda
-local fmt = require("luasnip.extras.fmt").fmt
-local fmta = require("luasnip.extras.fmt").fmta
--- local conds = require "luasnip.extras.expand_conditions"
-local postfix = require("luasnip.extras.postfix").postfix
--- local types = require "luasnip.util.types"
--- local parse = require("luasnip.util.parser").parse_snippet
--- local ms = ls.multi_snippet
-
+---@diagnostic disable: undefined-global
 return {
   s(
     { trig = "se", dscr = "add latex section" },
