1e294261c58021e16e1f3de70acbfb100d2a4621
Simone Fidanza <fsimone2002@proton.me>
2023-06-29 14:44:18 +0200

    feat(ui): add Lazy module to lualine

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -60,6 +60,11 @@ return {
         },
       },
       lualine_x = {
+        {
+          require("lazy.status").updates,
+          cond = require("lazy.status").has_updates,
+          color = { fg = "#ff9e64" },
+        },
         { "encoding" },
         {
           "fileformat",
