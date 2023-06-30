ca4117f875cb02bf20eba1de537d3e81412fbf25
Simone Fidanza <fsimone2002@proton.me>
2023-06-30 14:47:30 +0200

    feat(plugins): install toggleterm

---
+++ lua/plugins/editor/toggleterm.lua
@@ -0,0 +1,23 @@
+return {
+  -- A neovim lua plugin to help easily manage multiple terminal windows
+  "akinsho/toggleterm.nvim",
+  dependencies = {},
+  version = "*",
+  opts = {
+    size = function(term)
+      if term.direction == "horizontal" then
+        return 15
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.4
+      end
+    end,
+  },
+  direction = "float",
+  shell = function()
+    local shell = { win = "pwsh.exe", lnx = "bash" }
+    return shell[require("utils.fn").get_os()]
+  end,
+  float_opts = {
+    border = require("preferences").border,
+  },
+}
