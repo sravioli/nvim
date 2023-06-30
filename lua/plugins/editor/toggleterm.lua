a9873e4735c1d50ebbedfc1404c036044a8e07dc
Simone Fidanza <fsimone2002@proton.me>
2023-06-30 15:18:45 +0200

    fix(plugins): move toggleterm config inside `opts` table

--- lua/plugins/editor/toggleterm.lua
+++ lua/plugins/editor/toggleterm.lua
@@ -1,8 +1,11 @@
 return {
   -- A neovim lua plugin to help easily manage multiple terminal windows
   "akinsho/toggleterm.nvim",
-  dependencies = {},
+  event = "VeryLazy",
   version = "*",
+  init = function()
+    require("utils.fn").load_mappings "toggleterm"
+  end,
   opts = {
     size = function(term)
       if term.direction == "horizontal" then
@@ -11,13 +14,18 @@ return {
         return vim.o.columns * 0.4
       end
     end,
-  },
-  direction = "float",
-  shell = function()
-    local shell = { win = "pwsh.exe", lnx = "bash" }
-    return shell[require("utils.fn").get_os()]
-  end,
-  float_opts = {
-    border = require("preferences").border,
+    direction = "float",
+    shell = function()
+      local shell = { win = "pwsh.exe", lnx = "bash" }
+      return shell[require("utils.fn").get_os()]
+    end,
+    float_opts = {
+      border = require("preferences").border,
+    },
+    highlights = {
+      FloatBorder = {
+        link = "FloatBorder",
+      },
+    },
   },
 }
