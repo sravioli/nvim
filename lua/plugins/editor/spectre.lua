8c5f9734bb71d550f71c1f89790173e691ef5f34
Simone Fidanza <fsimone2002@proton.me>
2023-06-30 16:11:23 +0200

    feat(plugins): install nvim-spectre

---
+++ lua/plugins/editor/spectre.lua
@@ -0,0 +1,24 @@
+return {
+  -- Find the enemy and replace them with dark power.
+  "nvim-pack/nvim-spectre",
+  keys = {
+    { "<leader>S", ":lua require('spectre').open()<CR>", desc = "Open Spectre" },
+    {
+      "<leader>sw",
+      ":lua require('spectre').open_visual({select_word=true})<CR>",
+      desc = "Search current word",
+    },
+    {
+      "<leader>sw",
+      '<esc>:lua require("spectre").open_visual()<CR>',
+      desc = "Search current word",
+      mode = "v",
+    },
+    {
+      "<leader>sp",
+      ':lua require("spectre").open_file_search({select_word=true})<CR>',
+      desc = "Search on current file",
+    },
+  },
+  config = true,
+}
