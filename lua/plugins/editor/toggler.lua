c12d953c2b8c97ba200c25123174bcd74bda15d3
Simone Fidanza <fsimone2002@proton.me>
2023-06-30 22:00:28 +0200

    feat(plugins): install nvim-toggler

---
+++ lua/plugins/editor/toggler.lua
@@ -0,0 +1,44 @@
+return {
+  -- invert text in vim, purely with lua.
+  "nguyenvukhang/nvim-toggler",
+  keys = {
+    {
+      "<leader>i",
+      ":lua require('nvim-toggler').toggle()<CR>",
+      desc = "Toggle word",
+    },
+  },
+  opts = {
+    -- your own inverses
+    inverses = {
+      ["True"] = "False",
+      ["TRUE"] = "FALSE",
+      ["vim"] = "emacs",
+      ["TD"] = "LR",
+      ["input"] = "output",
+      ["enable"] = "disable",
+      ["east"] = "ovest",
+      ["north"] = "south",
+      ["sì"] = "no",
+      ["AND"] = "OR",
+      ["begin"] = "end",
+      ["up"] = "down",
+      ["left"] = "right",
+      ["hardware"] = "hardware",
+      ["for"] = "while",
+      ["int"] = "float",
+      ["=="] = "!=",
+      [">"] = "<",
+      ["+"] = "-",
+      ["*"] = "/",
+      [","] = ";",
+      ["."] = ":",
+      ["minimum"] = "maximum",
+      ["min"] = "max",
+    },
+    -- removes the default <leader>i keymap
+    remove_default_keybinds = true,
+    -- removes the default set of inverses
+    remove_default_inverses = false,
+  },
+}
