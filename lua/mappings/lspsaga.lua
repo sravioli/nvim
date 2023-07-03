837b489dbfcc2df54a459f3cb83fba9c3b69fb50
Simone Fidanza <fsimone2002@proton.me>
2023-07-03 15:27:40 +0200

    refactor(keymaps): use lspsaga lua api, add icons to descriptions

--- lua/mappings/lspsaga.lua
+++ lua/mappings/lspsaga.lua
@@ -2,38 +2,96 @@ return {
   plugin = true,
 
   n = {
-    ["<leader>fd"] = { "<cmd>Lspsaga lsp_finder<CR>", "Find symbol definition" },
-
-    ["<M-.>"] = { "<cmd>Lspsaga code_action<CR>", "Open code actions" },
-
-    ["<leader>rn"] = { "<cmd>Lspsaga rename<CR>", "Rename element" },
-
-    ["gd"] = { "<cmd>Lspsaga goto_definition<CR>", "Goto definition" },
-    ["gpd"] = { "<cmd>Lspsaga peek_definition<CR>", "Peek definition" },
-    ["gpt"] = {
-      "<cmd>Lspsaga peek_type_definition<CR>",
-      "Peek type definition",
+    ["<leader>fd"] = {
+      function()
+        require("lspsaga.finder"):lsp_finder()
+      end,
+      "  Lspsaga find definition",
     },
-    ["gtd"] = { "<cmd>Lspsaga goto_type_definition<CR>", "Goto type definition" },
 
-    ["K"] = { "<cmd>Lspsaga hover_doc<CR>", "On hover docs" },
+    ["<M-.>"] = {
+      function()
+        require("lspsaga.codeaction"):code_action()
+      end,
+      "󱐋  Lspsaga code actions",
+    },
+
+    ["<leader>rn"] = {
+      function()
+        require("lspsaga.rename"):lsp_rename()
+      end,
+      "󰑕   Lspsaga rename",
+    },
+
+    ["gd"] = {
+      function()
+        require("lspsaga.definition"):goto_definition(1)
+      end,
+      "󰞘  Lspsaga definition",
+    },
+    ["gT"] = {
+      function()
+        require("lspsaga.definition"):goto_definition(2)
+      end,
+      "   Lspsaga type definition",
+    },
+
+    ["gpd"] = {
+      function()
+        require("lspsaga.definition"):peek_definition(1)
+      end,
+      "   Lspsaga peek definition",
+    },
+    ["gpt"] = {
+      function()
+        require("lspsaga.definition"):goto_definition(2)
+      end,
+      "  Lspsaga peek type definition",
+    },
+
+    ["K"] = {
+      function()
+        require("lspsaga.hover"):render_hover_doc()
+      end,
+      "󱔗  Lspsaga hover docs",
+    },
+
     ["<Leader>ci"] = {
-      "<cmd>Lspsaga incoming_calls<CR>",
-      "Open incoming call hierarchy",
+      function()
+        require("lspsaga.callhierarchy"):send_method(2)
+      end,
+      "  Lspsaga incoming call hierarchy",
     },
     ["<Leader>co"] = {
-      "<cmd>Lspsaga outgoing_calls<CR>",
-      "Open outgoing call hierarchy",
+      function()
+        require("lspsaga.callhierarchy"):send_method(3)
+      end,
+      "   Lspsaga outgoing call hierarchy",
     },
 
-    ["<leader>o"] = { "<cmd>Lspsaga outline<CR>", "Open document outline" },
+    ["<leader>o"] = {
+      function()
+        require("lspsaga.outline"):outline()
+      end,
+      "󰧮  Lspsaga outline",
+    },
   },
 
   i = {
-    ["<M-.>"] = { "<cmd>Lspsaga code_action<CR>", "Open code actions" },
+    ["<M-.>"] = {
+      function()
+        require("lspsaga.codeaction"):code_action()
+      end,
+      "󱐋  Open code actions",
+    },
   },
 
   v = {
-    ["<M-.>"] = { "<cmd>Lspsaga code_action<CR>", "Open code actions" },
+    ["<M-.>"] = {
+      function()
+        require("lspsaga.codeaction"):code_action()
+      end,
+      "󱐋  Open code actions",
+    },
   },
 }
