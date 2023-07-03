2612993c3746f4d053dc1255ec70eae88deaad4f
Simone Fidanza <fsimone2002@proton.me>
2023-07-03 21:07:26 +0200

    fix(settings): load correct diagnostic signs

--- lua/settings.lua
+++ lua/settings.lua
@@ -100,8 +100,8 @@ for prov, paths in pairs(providers) do
   let[prov .. "_host_prog"] = paths[fn.get_os()]
 end
 
----Define diagnostic signs
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+---Load diagnostic signs
+local signs = require("preferences").signs
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
