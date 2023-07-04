87a9e7645a320157c75e48e59472dcd5a0678a7b
Simone Fidanza <fsimone2002@proton.me>
2023-07-04 23:45:06 +0200

    fix(keymaps): make buffer mappings use the `<leader>b` prefix

--- lua/srv/mappings/barbar.lua
+++ lua/srv/mappings/barbar.lua
@@ -7,25 +7,29 @@ return {
     ["<S-Tab>"] = { "<cmd>BufferPrevious<CR>", "󰒭  Next buffer", opts = opts },
     ["<Tab>"] = { "<cmd>BufferNext<CR>", "󰒮  Prev buffer", opts = opts },
     ---Re-order to previous/next
-    ["<A-<>"] = {
+    ["<leader>b<"] = {
       "<cmd>BufferMovePrevious<CR>",
       "󰁎  Move buffer left",
       opts = opts,
     },
-    ["<A->>"] = { "<cmd>BufferMoveNext<CR>", "  Move buffer right", opts = opts },
+    ["<leader>b>"] = {
+      "<cmd>BufferMoveNext<CR>",
+      "  Move buffer right",
+      opts = opts,
+    },
     ---Goto buffer in position...
-    ["<A-1>"] = { "<cmd>BufferGoto 1<CR>", "󰎤  Goto buffer 1", opts = opts },
-    ["<A-2>"] = { "<cmd>BufferGoto 2<CR>", "󰎧  Goto buffer 2", opts = opts },
-    ["<A-3>"] = { "<cmd>BufferGoto 3<CR>", "󰎪  Goto buffer 3", opts = opts },
-    ["<A-4>"] = { "<cmd>BufferGoto 4<CR>", "󰎭  Goto buffer 4", opts = opts },
-    ["<A-5>"] = { "<cmd>BufferGoto 5<CR>", "󰎱  Goto buffer 5", opts = opts },
-    ["<A-6>"] = { "<cmd>BufferGoto 6<CR>", "󰎳  Goto buffer 6", opts = opts },
-    ["<A-7>"] = { "<cmd>BufferGoto 7<CR>", "󰎶  Goto buffer 7", opts = opts },
-    ["<A-8>"] = { "<cmd>BufferGoto 8<CR>", "󰎹  Goto buffer 8", opts = opts },
-    ["<A-9>"] = { "<cmd>BufferGoto 9<CR>", "󰎼  Goto buffer 9", opts = opts },
-    ["<A-0>"] = { "<cmd>BufferLast<CR>", "󰘁  Goto last buffer", opts = opts },
+    ["<leader>b1"] = { "<cmd>BufferGoto 1<CR>", "󰎤  Goto buffer 1", opts = opts },
+    ["<leader>b2"] = { "<cmd>BufferGoto 2<CR>", "󰎧  Goto buffer 2", opts = opts },
+    ["<leader>b3"] = { "<cmd>BufferGoto 3<CR>", "󰎪  Goto buffer 3", opts = opts },
+    ["<leader>b4"] = { "<cmd>BufferGoto 4<CR>", "󰎭  Goto buffer 4", opts = opts },
+    ["<leader>b5"] = { "<cmd>BufferGoto 5<CR>", "󰎱  Goto buffer 5", opts = opts },
+    ["<leader>b6"] = { "<cmd>BufferGoto 6<CR>", "󰎳  Goto buffer 6", opts = opts },
+    ["<leader>b7"] = { "<cmd>BufferGoto 7<CR>", "󰎶  Goto buffer 7", opts = opts },
+    ["<leader>b8"] = { "<cmd>BufferGoto 8<CR>", "󰎹  Goto buffer 8", opts = opts },
+    ["<leader>b9"] = { "<cmd>BufferGoto 9<CR>", "󰎼  Goto buffer 9", opts = opts },
+    ["<leader>b0"] = { "<cmd>BufferLast<CR>", "󰘁  Goto last buffer", opts = opts },
     ---Pin/unpin buffer
-    ["<A-p>"] = { "<cmd>BufferPin<CR>", "  Pin buffer", opts = opts },
+    ["<leader>bP"] = { "<cmd>BufferPin<CR>", "  Pin buffer", opts = opts },
     ---Close buffer
     ["<leader>bx"] = { "<cmd>BufferClose<CR>", "  Close buffer", opts = opts },
     ---Wipeout buffer
