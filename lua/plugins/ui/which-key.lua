e675b768a33afae21253429f8912fb00534579a4
Simone Fidanza <fsimone2002@proton.me>
2023-07-04 11:07:39 +0200

    feat(ui): add which-key groups, and register them

--- lua/plugins/ui/which-key.lua
+++ lua/plugins/ui/which-key.lua
@@ -23,5 +23,26 @@ return {
       ["<cr>"] = "RET",
       ["<tab>"] = "TAB",
     },
+    defaults = {
+      mode = { "n", "v" },
+      ["g"] = { name = "+goto" },
+      ["gz"] = { name = "+surround" },
+      ["]"] = { name = "+next" },
+      ["["] = { name = "+prev" },
+      ["<leader>b"] = { name = "+buffer" },
+      ["<leader>c"] = { name = "+code" },
+      ["<leader>f"] = { name = "+file/find" },
+      ["<leader>g"] = { name = "+git" },
+      ["<leader>h"] = { name = "+hunks" },
+      ["<leader>q"] = { name = "+quit/session" },
+      ["<leader>s"] = { name = "+search" },
+      ["<leader>x"] = { name = "+diagnostics/quickfix" },
+      ["<leader>t"] = { name = "+toggle" },
+    },
   },
+  config = function(_, opts)
+    local wk = require "which-key"
+    wk.setup(opts)
+    wk.register(opts.defaults)
+  end,
 }
