a400cc9cca5c225595279faf07eb33eb5f52ad70
Simone Fidanza <fsimone2002@proton.me>
2023-07-06 11:57:55 +0200

    refactor(ui): move require inside the opts block, use preferences icons

--- lua/srv/plugins/ui/lualine.lua
+++ lua/srv/plugins/ui/lualine.lua
@@ -1,4 +1,3 @@
-local signs = require("srv.preferences").signs
 return {
   ---A blazing fast and easy to configure neovim statusline plugin written in
   ---pure lua.
@@ -9,135 +8,138 @@ return {
     { "linrongbin16/lsp-progress.nvim" },
   },
   event = "UIEnter",
-  opts = {
-    options = {
-      icons_enabled = true,
-      component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
-      disabled_filetypes = {
-        statusline = {},
-        winbar = {},
+  opts = function()
+    local icons = require("srv.preferences").icons
+    return {
+      options = {
+        icons_enabled = true,
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = true,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+        },
       },
-      ignore_focus = {},
-      always_divide_middle = true,
-      globalstatus = true,
-      refresh = {
-        statusline = 1000,
-        tabline = 1000,
-        winbar = 1000,
-      },
-    },
-    sections = {
-      lualine_a = {
-        { "mode" },
-      },
-      lualine_b = {
-        {
-          "filetype",
-          icon_only = true,
-          separator = "",
-          padding = {
-            left = 1,
-            right = 0,
+      sections = {
+        lualine_a = {
+          { "mode" },
+        },
+        lualine_b = {
+          {
+            "filetype",
+            icon_only = true,
+            separator = "",
+            padding = {
+              left = 1,
+              right = 0,
+            },
+          },
+          { "filename" },
+        },
+        lualine_c = {
+          { "b:gitsigns_head", icon = icons.git.Branch, color = "Comment" },
+          {
+            "diff",
+            symbols = {
+              added = icons.git.Added,
+              modified = icons.git.Modified,
+              removed = icons.git.Removed,
+            },
           },
         },
-        { "filename" },
-      },
-      lualine_c = {
-        { "b:gitsigns_head", icon = "  ", color = "Comment" },
-        {
-          "diff",
-          symbols = {
-            added = "  ",
-            modified = "  ",
-            removed = "  ",
+        lualine_x = {
+          {
+            "diagnostics",
+            sources = { "nvim_lsp", "nvim_diagnostic" },
+            symbols = {
+              error = " " .. icons.diagnostics.Error,
+              warn = " " .. icons.diagnostics.Warn,
+              info = " " .. icons.diagnostics.Info,
+              hint = " " .. icons.diagnostics.Hint,
+            },
           },
-        },
-      },
-      lualine_x = {
-        {
-          "diagnostics",
-          sources = { "nvim_lsp", "nvim_diagnostic" },
-          symbols = {
-            error = " " .. signs.Error,
-            warn = " " .. signs.Warn,
-            info = " " .. signs.Info,
-            hint = " " .. signs.Hint,
+          {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = { fg = "#ff9e64" },
           },
-        },
-        {
-          require("lazy.status").updates,
-          cond = require("lazy.status").has_updates,
-          color = { fg = "#ff9e64" },
-        },
-        {
-          function()
-            return require("lsp-progress").progress {
-              format = function(messages)
-                local icon = "  "
-                if #messages > 0 then
-                  return table.concat(messages, " ") .. icon
-                end
-
-                local clients = vim.lsp.get_active_clients()
-                if #clients <= 0 then
-                  return icon
-                else
-                  local client_names = {}
-                  for _, client in ipairs(clients) do
-                    if client and client.name ~= "" then
-                      local name = "[" .. client.name .. "]"
-                      if client.name == "null-ls" then
-                        name = "(󰟢)"
-                      end
-
-                      table.insert(client_names, name)
-                    end
+          {
+            function()
+              return require("lsp-progress").progress {
+                format = function(messages)
+                  local icon = "  "
+                  if #messages > 0 then
+                    return table.concat(messages, " ") .. icon
                   end
-                  return table.concat(client_names, " ") .. icon
-                end
-              end,
-            }
-          end,
-          color = "Comment",
-        },
-        { "encoding" },
-        {
-          "fileformat",
-          icons_enabled = true,
-          symbols = {
-            unix = "LF",
-            dos = "CRLF",
-            mac = "CR",
+
+                  local clients = vim.lsp.get_active_clients()
+                  if #clients <= 0 then
+                    return icon
+                  else
+                    local client_names = {}
+                    for _, client in ipairs(clients) do
+                      if client and client.name ~= "" then
+                        local name = "[" .. client.name .. "]"
+                        if client.name == "null-ls" then
+                          name = "(" .. icons.kinds.Null .. ")"
+                        end
+
+                        table.insert(client_names, name)
+                      end
+                    end
+                    return table.concat(client_names, " ") .. icon
+                  end
+                end,
+              }
+            end,
+            color = "Comment",
+          },
+          { "encoding" },
+          {
+            "fileformat",
+            icons_enabled = true,
+            symbols = {
+              unix = "LF",
+              dos = "CRLF",
+              mac = "CR",
+            },
           },
         },
+        lualine_y = {
+          { "progress" },
+        },
+        lualine_z = {
+          { "location" },
+        },
       },
-      lualine_y = {
-        { "progress" },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
       },
-      lualine_z = {
-        { "location" },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {
+        "lazy",
+        "neo-tree",
+        "trouble",
+        "man",
+        "fzf",
+        "quickfix",
+        "toggleterm",
       },
-    },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = { "filename" },
-      lualine_x = { "location" },
-      lualine_y = {},
-      lualine_z = {},
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {
-      "lazy",
-      "neo-tree",
-      "trouble",
-      "man",
-      "fzf",
-      "quickfix",
-      "toggleterm",
-    },
-  },
+    }
+  end,
 }
