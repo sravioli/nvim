1130660b58b974c115fbdf6f334e368a82c38c05
Simone Fidanza <fsimone2002@proton.me>
2023-07-06 11:46:56 +0200

    feat(dap): load dap-ui on keymaps, add config

--- lua/srv/plugins/dap/dap-ui.lua
+++ lua/srv/plugins/dap/dap-ui.lua
@@ -1,8 +1,36 @@
 return {
   ---A UI for nvim-dap
   "rcarriga/nvim-dap-ui",
-  enabled = false,
-  dependencies = { "mfussenegger/nvim-dap" },
-  config = true,
-  event = "LspAttach",
+  keys = {
+    {
+      "<leader>du",
+      function()
+        require("dapui").toggle {}
+      end,
+      desc = "  Dap UI",
+    },
+    {
+      "<leader>de",
+      function()
+        require("dapui").eval()
+      end,
+      desc = "󰜎  Eval",
+      mode = { "n", "v" },
+    },
+  },
+  opts = {},
+  config = function(_, opts)
+    local dap = require "dap"
+    local dapui = require "dapui"
+    dapui.setup(opts)
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      dapui.open {}
+    end
+    dap.listeners.before.event_terminated["dapui_config"] = function()
+      dapui.close {}
+    end
+    dap.listeners.before.event_exited["dapui_config"] = function()
+      dapui.close {}
+    end
+  end,
 }
