9f3a334452097b15c2e15ae1ebbfd3a2b052784d
Simone Fidanza <fsimone2002@proton.me>
2023-07-06 11:56:06 +0200

    fix(ui): use preferences diagnostic icons for barbar

--- lua/srv/plugins/ui/barbar.lua
+++ lua/srv/plugins/ui/barbar.lua
@@ -11,52 +11,55 @@ return {
     vim.g.barbar_auto_setup = false
     require("srv.utils.fn").mappings.load "barbar"
   end,
-  opts = {
-    ---Enable/disable animations
-    animation = false,
+  opts = function()
+    local signs = require("srv.preferences").icons.diagnostics
+    return {
+      ---Enable/disable animations
+      animation = false,
 
-    ---Enable/disable auto-hiding the tab bar when there is a single buffer
-    auto_hide = true,
+      ---Enable/disable auto-hiding the tab bar when there is a single buffer
+      auto_hide = true,
 
-    ---Hide inactive buffers and file extensions. Other options are `alternate`, `current`, and `visible`.
-    hide = { extensions = true, inactive = false },
+      ---Hide inactive buffers and file extensions. Other options are `alternate`, `current`, and `visible`.
+      hide = { extensions = true, inactive = false },
 
-    ---Enable highlighting visible buffers
-    highlight_visible = false,
+      ---Enable highlighting visible buffers
+      highlight_visible = false,
 
-    icons = {
-      ---Configure the base icons on the bufferline.
-      ---Valid options to display the buffer index and -number are `true`, 'superscript' and 'subscript'
-      buffer_index = false,
-      buffer_number = false,
-      button = "×",
-      ---Enables / disables diagnostic symbols
-      diagnostics = {
-        [vim.diagnostic.severity.ERROR] = { enabled = true, icon = " " },
-        [vim.diagnostic.severity.WARN] = { enabled = true, icon = " " },
-        [vim.diagnostic.severity.INFO] = { enabled = true, icon = " " },
-        [vim.diagnostic.severity.HINT] = { enabled = true, icon = " " },
+      icons = {
+        ---Configure the base icons on the bufferline.
+        ---Valid options to display the buffer index and -number are `true`, 'superscript' and 'subscript'
+        buffer_index = false,
+        buffer_number = false,
+        button = "×",
+        ---Enables / disables diagnostic symbols
+        diagnostics = {
+          [vim.diagnostic.severity.ERROR] = { enabled = true, icon = signs.Error },
+          [vim.diagnostic.severity.WARN] = { enabled = true, icon = signs.Warn },
+          [vim.diagnostic.severity.INFO] = { enabled = true, icon = signs.Info },
+          [vim.diagnostic.severity.HINT] = { enabled = true, icon = signs.Hint },
+        },
+        gitsigns = {
+          added = { enabled = true, icon = "+" },
+          changed = { enabled = true, icon = "~" },
+          deleted = { enabled = true, icon = "-" },
+        },
+
+        ---Configure the icons on the bufferline when modified or pinned.
+        ---Supports all the base icon options.
+        modified = { button = "●" },
+        pinned = { button = "", filename = true },
+
+        ---Use a preconfigured buffer appearance— can be 'default', 'powerline', or 'slanted'
+        preset = "slanted",
+
+        ---Configure the icons on the bufferline based on the visibility of a buffer.
+        ---Supports all the base icon options, plus `modified` and `pinned`.
+        alternate = { filetype = { enabled = false } },
+        current = { buffer_index = false },
+        inactive = { button = "󰒲" },
+        visible = { modified = { buffer_number = true } },
       },
-      gitsigns = {
-        added = { enabled = true, icon = "+" },
-        changed = { enabled = true, icon = "~" },
-        deleted = { enabled = true, icon = "-" },
-      },
-
-      ---Configure the icons on the bufferline when modified or pinned.
-      ---Supports all the base icon options.
-      modified = { button = "●" },
-      pinned = { button = "", filename = true },
-
-      ---Use a preconfigured buffer appearance— can be 'default', 'powerline', or 'slanted'
-      preset = "slanted",
-
-      ---Configure the icons on the bufferline based on the visibility of a buffer.
-      ---Supports all the base icon options, plus `modified` and `pinned`.
-      alternate = { filetype = { enabled = false } },
-      current = { buffer_index = false },
-      inactive = { button = "󰒲" },
-      visible = { modified = { buffer_number = true } },
-    },
-  },
+    }
+  end,
 }
