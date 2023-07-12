699706651bbd26b08e4e96b6ff5deaf4844a10be
Simone Fidanza <fsimone2002@proton.me>
2023-07-12 12:52:16 +0200

    fix(plugins): typo in lsp-progress declaration

--- lua/srv/plugins/lsp/post/lsp-progress.lua
+++ lua/srv/plugins/lsp/post/lsp-progress.lua
@@ -2,6 +2,6 @@ return {
   ---A performant LSP progress status for Neovim.
   "linrongbin16/lsp-progress.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  evet = "LspAttach",
+  event = "LspAttach",
   config = true,
 }
