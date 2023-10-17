bfdbb24b3833507a6b9b99c5f3a81e0e7c0b0f8b
Simone Fidanza <fsimone2002@proton.me>
2023-10-17 10:25:00 +0200

    feat(snippets:toml): add stylua config as toml snippet

---
+++ lua/srv/snippets/toml.lua
@@ -0,0 +1,30 @@
+---@diagnostic disable: undefined-global
+return {
+  s(
+    { trig = "stylua", dscr = "stylua default config" },
+    fmt(
+      [[
+        column_width = {column_width}
+        line_endings = "{line_endings}"
+        indent_type = "{indent_type}"
+        indent_width = {indent_width}
+        quote_style = "{quote_style}"
+        call_parentheses = "{call_parentheses}"
+        collapse_simple_statement = "{collapse_simple_statement}"
+
+        [sort_requires]
+        enabled = {sort_requires}
+      ]],
+      {
+        column_width = i(1, "90"),
+        line_endings = i(2, "Windows"),
+        indent_type = i(3, "Spaces"),
+        indent_width = i(4, "2"),
+        quote_style = i(5, "AutoPreferDouble"),
+        call_parentheses = i(6, "None"),
+        collapse_simple_statement = i(7, "Always"),
+        sort_requires = i(8, "false"),
+      }
+    )
+  ),
+}
