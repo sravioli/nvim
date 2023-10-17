afc9ba0f20936944b65772f1de2b0b1cf3ad9093
Simone Fidanza <fsimone2002@proton.me>
2023-10-17 10:10:43 +0200

    feat(plugins:motions): install `surround-ui`

--- lua/srv/plugins/motions/surround.lua
+++ lua/srv/plugins/motions/surround.lua
@@ -1,7 +1,23 @@
 return {
-  ---Add/change/delete surrounding delimiter pairs with ease. Written with ❤️ in Lua.
-  "kylechui/nvim-surround",
-  -- tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-  config = true,
-  event = "BufReadPost",
+  {
+    ---Add/change/delete surrounding delimiter pairs with ease. Written with ❤️ in Lua.
+    "kylechui/nvim-surround",
+    -- tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = true,
+    event = "BufReadPost",
+  },
+  {
+    ---🤗 A Neovim plugin which acts as a helper or training aid for nvim-surround
+    "roobert/surround-ui.nvim",
+    event = "BufReadPost",
+    dependencies = {
+      { "kylechui/nvim-surround" },
+      { "folke/which-key.nvim" },
+    },
+    config = function()
+      require("surround-ui").setup {
+        root_key = "S",
+      }
+    end,
+  },
 }
