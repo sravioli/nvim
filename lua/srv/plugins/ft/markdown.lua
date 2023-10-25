e4b415eb901450450df3d7f87b53bf6badbb4b57
Simone Fidanza <fsimone2002@proton.me>
2023-10-25 16:42:39 +0200

    feat(plugins:ft): install `nabla.nvim`

--- lua/srv/plugins/ft/markdown.lua
+++ lua/srv/plugins/ft/markdown.lua
@@ -136,47 +136,24 @@ return {
         local buf = vim.api.nvim_create_buf(false, false)
         return vim.api.nvim_open_win(buf, true, opts)
       end,
-      -- should return options passed to nvim_open_win
-      -- @param code_block: data about the code-block with the keys
-      --   * range
-      --   * lines
-      --   * lang
-      -- float_opts = function(code_block)
-      --   return {
-      --     relative = "cursor",
-      --     width = clip_val(5, 120, vim.api.nvim_win_get_width(0) - 10), -- TODO how to offset sign column etc?
-      --     height = clip_val(
-      --       5,
-      --       #code_block.lines,
-      --       vim.api.nvim_win_get_height(0) - 6
-      --     ),
-      --     anchor = "NW",
-      --     row = 0,
-      --     col = 0,
-      --     style = "minimal",
-      --     border = "rounded",
-      --     zindex = 1,
-      --   }
-      -- end,
-      -- return filetype to use for a given lang
-      -- lang can be nil
-      -- ft_from_lang = function(lang)
-      --   return lang
-      -- end,
-      -- what to do after opening the float
-      -- post_open_float = function(winnr)
-      --   vim.wo.signcolumn = "no"
-      -- end,
-      -- create the path to a temporary file
-      -- create_tmp_filepath = function(filetype)
-      --   return os.tmpname()
-      -- end,
+
       -- if a newline should always be used, useful for multiline injections
       -- which separators needs to be on separate lines such as markdown, neorg etc
       -- @param base_filetype: The filetype which FeMaco is called from, not the
       -- filetype of the injected language (this is the current buffer so you can
       -- get it from vim.bo.filetyp).
       ensure_newline = function(base_filetype) return true end,
+    },
+  },
+  { ---take your scientific notes ✏️ in Neovim
+    "jbyuki/nabla.nvim",
+    ft = "markdown",
+    keys = {
+      {
+        "<leader>cp",
+        function() require("nabla").popup { border = require("srv.preferences").border } end,
+        "∇  Open Nabla popup",
+      },
     },
   },
 }
