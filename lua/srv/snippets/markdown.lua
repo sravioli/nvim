087fda3f62f1a2e47e67237931d6748cad77a453
Simone Fidanza <fsimone2002@proton.me>
2023-11-06 15:35:35 +0100

    feat(snippets:markdown): add snippet to quickly add obsidian metadata to university notes

--- lua/srv/snippets/markdown.lua
+++ lua/srv/snippets/markdown.lua
@@ -22,7 +22,7 @@ return {
       > [!note] {title}
       >
       > {body}
-      
+
 
       ]],
       { title = i(1, "Nota"), body = i(2) }
@@ -114,7 +114,7 @@ return {
       > [!failure] {title}
       >
       > {body}
-      
+
       ]],
       { title = i(1, "Errore"), body = i(2) }
     )
@@ -153,7 +153,7 @@ return {
       > [!example] {title}
       >
       > {body}
-      
+
       ]],
       { title = i(1, "Esempio"), body = i(2) }
     )
@@ -176,13 +176,13 @@ return {
     fmt(
       [[
       ---
-      aliases: 
+      aliases:
         - {aliases}
       tags:
         - {tag_camel_case}
         - {tag_initials}
       title: {title}
-      description: >- 
+      description: >-
         {desc}
       author: sRavioli
       created: {date}
@@ -202,12 +202,81 @@ return {
   ),
 
   s(
+    { trig = "unimetadata", dscr = "Add Obsidian metadata for university notes" },
+    fmt(
+      [=[
+      ---
+      professore:
+        {professore}, {email}
+      ricevimento: {ricevimento}
+      lezioni:
+        {lezioni}
+      esonero: {esonero}
+      esame:
+        pratico: {esame_pratico}
+        scritto: {esame_scritto}
+        orale: {esame_orale}
+      CFU: {cfu}
+      e-learning:
+        uri: "https://elearning.uniba.it/course/view.php?id={elearning_id}"
+        passwd: {elearning_passwd}
+      libri:
+        - "[[{libri}]]"{altri_libri}
+      tags:
+        - {tag_camel_case}
+        - {tag_initials}
+      aliases:
+        - {aliases}{altri_alias}
+      title: {title}
+      description: >-
+        {description}
+      author: sRavioli
+      created: {date_created}
+      updated: {date_modified}
+      ---
+
+      ]=],
+      {
+        professore = i(1, "Nome Cognome"),
+        email = f(function(args, _, _)
+          -- local name, surname, second_surname =
+          --   args[1][1]:lower():match "^(%w*)%s*(%w*)%s*(%w*)$"
+          -- return string.format("%s.%s%s@uniba.it", name, surname, second_surname)
+          return string.format(
+            "%s.%s%s@uniba.it",
+            args[1][1]:lower():match "^(%w*)%s*(%w*)%s*(%w*)$"
+          )
+        end, { 1 }),
+        ricevimento = i(2, "orari di ricevimento"),
+        lezioni = i(3, "giorno: orario"),
+        esonero = c(4, { t { "presente" }, t { "assente" }, i(nil, "altro") }),
+        esame_pratico = i(5, "~"),
+        esame_scritto = i(6, "~"),
+        esame_orale = i(7, "~"),
+        cfu = i(8, "7+2"),
+        elearning_id = i(9, "id"),
+        elearning_passwd = i(10, "passwd"),
+        libri = i(11, "./books/libro.pdf"),
+        altri_libri = i(12),
+        tag_camel_case = i(13, "TagsDellaNota"),
+        tag_initials = l(l._1:gsub("%l", ""):gsub("%W", ""), 13),
+        aliases = l("scheda " .. l._1:gsub("%l", ""):gsub("%W", ""), 13),
+        altri_alias = i(14),
+        title = i(15, "titolo"),
+        description = i(16, "descrizione"),
+        date_created = f(function() return os.date "%Y-%m-%d %H:%M" end),
+        date_modified = f(function() return os.date "%Y-%m-%d %H:%M" end),
+      }
+    )
+  ),
+
+  s(
     { trig = "cpsex", dscr = "Esercizio di Calcolo di Probabilità e Statistica" },
     fmt(
       [[
       **Traccia**: {}
 
-      **Breve**: 
+      **Breve**:
 
       **Svolgimento**: {}
     ]],
