f6a0b06645498f9e08ccf79f82c8f74ffd75a742
Simone Fidanza <fsimone2002@proton.me>
2023-11-07 22:42:03 +0100

    chore(plugins:ui): inline dependencies for `dropbar.nvim`

--- lua/srv/plugins/ui/dropbar.lua
+++ lua/srv/plugins/ui/dropbar.lua
@@ -3,8 +3,5 @@ return {
   "Bekaboo/dropbar.nvim",
   enabled = false, ---activate when neovim version >= 0.10.0
   event = "VeryLazy",
-  -- optional, but required for fuzzy finder support
-  dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-  },
+  dependencies = { { "nvim-telescope/telescope-fzf-native.nvim" } },
 }
