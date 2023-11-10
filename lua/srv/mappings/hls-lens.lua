d72bca958eb0afb860d90473c67fc45375bbeac9
Simone Fidanza <fsimone2002@proton.me>
2023-11-10 13:33:54 +0100

    feat(mappings): add hls-lens keymaps

---
+++ lua/srv/mappings/hls-lens.lua
@@ -0,0 +1,38 @@
+---@class Mappings
+return {
+  autoload = false,
+
+  n = {
+    ["n"] = { "nzz<cmd>lua require('hlslens').start()<CR>" },
+    ["N"] = { "Nzz<cmd>lua require('hlslens').start()<CR>" },
+
+    ["*"] = { "*zz<cmd>lua require('hlslens').start()<CR>" },
+    ["#"] = { "#zz<cmd>lua require('hlslens').start()<CR>" },
+    ["g*"] = { "g*zz<cmd>lua require('hlslens').start()<CR>" },
+    ["g#"] = { "g#zz<cmd>lua require('hlslens').start()<CR>" },
+
+    ["<leader>L"] = {
+      function()
+        vim.schedule(function()
+          if require("hlslens").exportLastSearchToQuickfix() then vim.cmd "cwindow" end
+        end)
+        return ":nohlsearch<CR>"
+      end,
+      "  export search results to quickfix",
+      opts = { expr = true },
+    },
+  },
+
+  x = {
+    ["<leader>L"] = {
+      function()
+        vim.schedule(function()
+          if require("hlslens").exportLastSearchToQuickfix() then vim.cmd "cwindow" end
+        end)
+        return ":nohlsearch<CR>"
+      end,
+      "  export search results to quickfix",
+      opts = { expr = true },
+    },
+  },
+}
