cf8d055d86509b644e6251c2af5b96f5e83de891
Simone Fidanza <fsimone2002@proton.me>
2023-11-13 18:32:14 +0100

    fix(plugins:ui): remove unsupported telescope `layout_config` option

--- lua/srv/plugins/ui/telescope.lua
+++ lua/srv/plugins/ui/telescope.lua
@@ -60,7 +60,6 @@ return {
         layout_config = {
           horizontal = {
             preview_width = 0.55,
-            results_width = 0.8,
           },
           vertical = {
             mirror = false,
