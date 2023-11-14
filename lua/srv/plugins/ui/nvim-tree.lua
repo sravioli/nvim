0b7b91e991c27afffe0cefc331ab0e57e947b790
Simone Fidanza <fsimone2002@proton.me>
2023-11-14 11:14:49 +0100

    chore(lazy): update `lazy.nvim` lockfile

--- lua/srv/plugins/ui/nvim-tree.lua
+++ lua/srv/plugins/ui/nvim-tree.lua
@@ -5,5 +5,158 @@ return {
   dependencies = {
     { "nvim-tree/nvim-web-devicons" },
   },
-  opts = {},
+  opts = function()
+    ---@class Icons
+    local icons = require("srv.preferences").icons
+    local border = require("srv.preferences").border
+
+    return {
+      ---to set nvim-tree specific mappings
+      on_attach = function(bufnr)
+        local api = require "nvim-tree.api"
+        local flprv = require "srv.utils.nvim-tree.float-preview"
+
+        flprv.on_attach(bufnr)
+        local float_close_wrap = flprv.close_wrap
+
+        local function opts(desc)
+          return {
+            desc = "nvim-tree: " .. desc,
+            buffer = bufnr,
+            noremap = true,
+            silent = true,
+            nowait = true,
+          }
+        end
+
+        --- There are keymaps must to wrap for correct work
+        -- ...
+        vim.keymap.set(
+          "n",
+          "<C-t>",
+          float_close_wrap(api.node.open.tab),
+          opts "Open: New Tab"
+        )
+        vim.keymap.set(
+          "n",
+          "<C-v>",
+          float_close_wrap(api.node.open.vertical),
+          opts "Open: Vertical Split"
+        )
+        vim.keymap.set(
+          "n",
+          "<C-s>",
+          float_close_wrap(api.node.open.horizontal),
+          opts "Open: Horizontal Split"
+        )
+        vim.keymap.set("n", "<CR>", float_close_wrap(api.node.open.edit), opts "Open")
+        vim.keymap.set("n", "<Tab>", float_close_wrap(api.node.open.preview), opts "Open")
+        vim.keymap.set("n", "o", float_close_wrap(api.node.open.edit), opts "Open")
+        vim.keymap.set(
+          "n",
+          "O",
+          float_close_wrap(api.node.open.no_window_picker),
+          opts "Open: No Window Picker"
+        )
+        vim.keymap.set("n", "q", float_close_wrap(api.tree.close), opts "Close")
+        vim.keymap.set("n", "a", float_close_wrap(api.fs.create), opts "Create")
+        vim.keymap.set("n", "d", float_close_wrap(api.fs.remove), opts "Delete")
+        vim.keymap.set("n", "r", float_close_wrap(api.fs.rename), opts "Rename")
+      end,
+      hijack_cursor = true, ---keep cursor on first letter of filename
+      disable_netrw = true, ---completely disable netrw
+      hijack_unnamed_buffer_when_opening = true, ---open in empty unnamed buffers
+      sync_root_with_cwd = true, ---change cwd on `DirChanged`
+      reload_on_bufenter = true, ---update on `BufEnter`
+      select_prompts = true, ---`vim.ui.select` prompts
+
+      view = {
+        cursorline = true,
+        side = "right",
+        -- number = true,
+        -- relativenumber = true,
+        signcolumn = "auto",
+
+        width = { padding = 2 },
+
+        float = {
+          enable = true,
+          open_win_config = {
+            relative = "editor",
+            border = border,
+            width = math.floor(vim.opt.columns:get() * 0.25),
+            height = 30,
+            row = 1,
+            col = 1,
+          },
+        },
+      },
+
+      renderer = {
+        group_empty = true,
+        full_name = true,
+        root_folder_label = function(root_cwd)
+          return root_cwd:gsub(os.getenv "USERPROFILE", "~"):gsub("\\", "/") .. "/"
+        end,
+
+        highlight_git = true,
+        highlight_diagnostics = true,
+        highlight_opened_files = "name",
+
+        indent_markers = {
+          enable = true,
+          inline_arrows = true,
+          icons = { corner = "╰" },
+        },
+
+        icons = {
+          web_devicons = { folder = { enable = true } },
+
+          git_placement = "signcolumn",
+          diagnostics_placement = "after",
+          symlink_arrow = " 󰁔 ",
+
+          glyphs = {
+            git = {
+              unstaged = icons.git.Unstaged,
+              staged = icons.git.Staged,
+              renamed = icons.git.Renamed,
+              untracked = icons.git.Untracked,
+              deleted = icons.git.Deleted,
+              ignored = icons.git.Ignored,
+            },
+          },
+        },
+      },
+
+      hijack_directories = { enable = true },
+
+      diagnostics = {
+        enable = true,
+        icons = {
+          hint = icons.diagnostics.Hint,
+          info = icons.diagnostics.Info,
+          warning = icons.diagnostics.Warn,
+          error = icons.diagnostics.Error,
+        },
+      },
+
+      modified = { enable = true },
+
+      filters = {
+        dotfiles = true,
+        exclude = { ".gitignore", ".github" },
+      },
+
+      actions = {
+        file_popup = {
+          open_win_config = {
+            border = border,
+            col = 1,
+            row = 1,
+          },
+        },
+      },
+    }
+  end,
 }
