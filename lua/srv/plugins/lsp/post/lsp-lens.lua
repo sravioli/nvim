e6b62af4051b3c5b82b2ce6745b38da03d95eeaa
Simone Fidanza <fsimone2002@proton.me>
2023-11-18 11:02:51 +0100

    feat(plugins:lsp): customize `lsp-lens` icons

--- lua/srv/plugins/lsp/post/lsp-lens.lua
+++ lua/srv/plugins/lsp/post/lsp-lens.lua
@@ -1,14 +1,19 @@
 return {
-  ---Neovim plugin for displaying references and definition infos upon functions like JB's IDEA.
+  ---Neovim plugin for displaying references and definition infos upon functions like JB's
+  ---IDEA.
   "VidocqH/lsp-lens.nvim",
   event = "LspAttach",
+  cmd = { "LspLensOn", "LspLensOff", "LspLensToggle" },
   opts = {
     enable = true,
     include_declaration = true, -- Reference include declaration
     sections = { -- Enable/Disable specific request
-      definition = true,
-      references = true,
-      implementation = true,
+      definition = function(count) return " " .. count end,
+      references = function(count) return " " .. count end,
+      implements = function(count) return " " .. count end,
+      git_authors = function(latest_author, count)
+        return "  " .. latest_author .. (count - 1 == 0 and "" or " +" .. count - 1)
+      end,
     },
   },
 }
