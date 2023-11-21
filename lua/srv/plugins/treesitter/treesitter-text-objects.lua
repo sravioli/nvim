ab49d5f29751e0887faaab7ad6ade108a49e9efc
Simone Fidanza <fsimone2002@proton.me>
2023-11-21 10:10:44 +0100

    fix(plugins:treesitter): change load event for treesitter

--- lua/srv/plugins/treesitter/treesitter-text-objects.lua
+++ lua/srv/plugins/treesitter/treesitter-text-objects.lua
@@ -1,6 +1,8 @@
 return {
   ---Syntax aware text-objects, select, move, swap, and peek support.
   "nvim-treesitter/nvim-treesitter-textobjects",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  event = "BufWinEnter",
+  dependencies = {
+    { "nvim-treesitter/nvim-treesitter" },
+  },
+  event = require("srv.utils.event").events.LazyFile,
 }
