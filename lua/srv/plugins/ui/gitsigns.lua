09afcf01bd6fec7bc70c58796408085d4812ca5f
Simone Fidanza <fsimone2002@proton.me>
2023-11-21 10:14:51 +0100

    fix: correctly lazy load gitsigns

--- lua/srv/plugins/ui/gitsigns.lua
+++ lua/srv/plugins/ui/gitsigns.lua
@@ -1,23 +1,7 @@
 return {
   ---Git integration for buffers
   "lewis6991/gitsigns.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  init = function()
-    ---load gitsigns only when a git file is opened
-    vim.api.nvim_create_autocmd({ "BufRead" }, {
-      group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
-      callback = function()
-        vim.fn.system("git -C " .. '"' .. vim.fn.expand "%:p:h" .. '"' .. " rev-parse")
-        if vim.v.shell_error == 0 then
-          vim.api.nvim_del_augroup_by_name "GitSignsLazyLoad"
-          vim.schedule(function()
-            require("lazy").load { plugins = { "gitsigns.nvim" } }
-          end)
-        end
-      end,
-    })
-  end,
-
+  event = require("srv.utils.event").events.LazyFile,
   opts = {
     signs = {
       add = { text = "│" },
@@ -27,11 +11,15 @@ return {
       changedelete = { text = "~" },
       untracked = { text = "│" },
     },
-    preview_config = {
-      border = require("srv.preferences").border,
-    },
+    preview_config = { border = require("srv.preferences").border },
+
     on_attach = function(bufnr)
       require("srv.utils.keymaps").load("gitsigns", { buffer = bufnr })
     end,
   },
+
+  -- config = function(_, opts)
+  --   require("gitsigns").setup(opts)
+  --   require("srv.utils.keymaps").load("gitsigns", { buffer = true })
+  -- end,
 }
