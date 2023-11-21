01437b843955618d9cfcfff5a962ac57d6f897b1
Simone Fidanza <fsimone2002@proton.me>
2023-11-21 10:30:12 +0100

    fix(lsp): move lspconfig to its file

--- lua/srv/plugins/lsp/servers/init.lua
+++ lua/srv/plugins/lsp/servers/init.lua
@@ -1,38 +1,18 @@
-local present, lspconfig = pcall(require, "lspconfig")
-if not present then
-  vim.notify "could not load lspconfig"
-  return
-end
+---@class (exact) LspServers
+---@field lua_ls   table
+---@field clangd   table
+---@field jsonls   table
+---@field marksman table
+---@field taplo    table
+---@field texlab   table
+---@field yamlls   table
+local M = {}
 
-local configs = require "srv.plugins.lsp.servers.configs"
+setmetatable(M, {
+  __index = function(t, k)
+    t[k] = require("srv.plugins.lsp.servers." .. k)
+    return t[k]
+  end,
+})
 
-local servers = {
-  "lua_ls",
-  "marksman",
-  "jsonls",
-  "taplo",
-  "yamlls",
-  "clangd",
-  "texlab",
-  -- "powershell_es",
-}
-
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = configs.on_attach,
-    capabilities = configs.capabilities(),
-    handlers = configs.handlers,
-
-    settings = {
-      Lua = require "srv.plugins.lsp.servers.lua_ls",
-      texlab = require "srv.plugins.lsp.servers.texlab",
-    },
-
-    marksman = require "srv.plugins.lsp.servers.marksman",
-    jsonls = require "srv.plugins.lsp.servers.jsonls",
-    taplo = require "srv.plugins.lsp.servers.taplo",
-    yamlls = require "srv.plugins.lsp.servers.yamlls",
-    clangd = require "srv.plugins.lsp.servers.clangd",
-    -- powershell_es = require "srv.plugins.lsp.servers.powershell_es",
-  }
-end
+return M
