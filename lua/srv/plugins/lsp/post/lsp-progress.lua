c82ddab6e8a814f5e9f69b4536c22f9800eed48d
Simone Fidanza <fsimone2002@proton.me>
2023-11-21 10:33:38 +0100

    fix(lsp:post): uninstall `lsp-progress`

--- lua/srv/plugins/lsp/post/lsp-progress.lua
+++
@@ -1,7 +0,0 @@
-return {
-  ---A performant LSP progress status for Neovim.
-  "linrongbin16/lsp-progress.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  event = "LspAttach",
-  opts = {},
-}
