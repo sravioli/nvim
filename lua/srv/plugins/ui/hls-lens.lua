84921875562607f9f3deb9151c5b67d8241a3f18
Simone Fidanza <fsimone2002@proton.me>
2023-11-21 10:15:27 +0100

    style(plugins:ui): inline table for `hls-lens` config

--- lua/srv/plugins/ui/hls-lens.lua
+++ lua/srv/plugins/ui/hls-lens.lua
@@ -2,9 +2,7 @@ return {
   ---Hlsearch Lens for Neovim
   "kevinhwang91/nvim-hlslens",
   event = "CmdlineEnter",
-  opts = {
-    calm_down = true,
-  },
+  opts = { calm_down = true },
   config = function(_, opts)
     require("scrollbar.handlers.search").setup(opts)
     require("srv.utils.keymaps").load("hls-lens", { noremap = true, silent = true })
