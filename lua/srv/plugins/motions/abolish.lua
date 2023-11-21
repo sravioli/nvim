68f5c926fa99c381b883823275e4d20a843b5aa8
Simone Fidanza <fsimone2002@proton.me>
2023-11-21 10:10:01 +0100

    feat(plugins:motions): better lazy loading for `vim-abolish`

--- lua/srv/plugins/motions/abolish.lua
+++ lua/srv/plugins/motions/abolish.lua
@@ -1,5 +1,6 @@
 return {
   ---abolish.vim: Work with several variants of a word at once.
   "tpope/vim-abolish",
-  event = "CmdlineEnter",
+  cmd = { "Subvert", "Abolish" },
+  keys = { { "crs" }, { "crm" }, { "crc" }, { "cru" }, { "cr-" }, { "cr." } },
 }
