ae1b9758edfa82696c47885a1375f24cc22f382e
Simone Fidanza <fsimone2002@proton.me>
2023-11-22 16:18:17 +0100

    feat(plugins:motions): lazy load `nvim-surround` on keymap, uninstall ui

--- lua/srv/plugins/motions/surround.lua
+++ lua/srv/plugins/motions/surround.lua
@@ -1,23 +1,19 @@
+--# selene: allow(mixed_table)
 return {
-  {
-    ---Add/change/delete surrounding delimiter pairs with ease. Written with ❤️ in Lua.
-    "kylechui/nvim-surround",
-    -- tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-    config = true,
-    event = "BufReadPost",
+  ---Add/change/delete surrounding delimiter pairs with ease. Written with ❤️ in Lua.
+  "kylechui/nvim-surround",
+  keys = {
+    { "ys" },
+    { "yS" },
+    { "yss" },
+    { "ySS" },
+    { "ds" },
+    { "cs" },
+    { "cS" },
+    { "S", mode = "v" },
+    { "gS", mode = "v" },
+    { "<C-g>s", mode = "i" },
+    { "<C-g>S", mode = "i" },
   },
-  {
-    ---🤗 A Neovim plugin which acts as a helper or training aid for nvim-surround
-    "roobert/surround-ui.nvim",
-    event = "BufReadPost",
-    dependencies = {
-      { "kylechui/nvim-surround" },
-      { "folke/which-key.nvim" },
-    },
-    config = function()
-      require("surround-ui").setup {
-        root_key = "S",
-      }
-    end,
-  },
+  opts = {},
 }
