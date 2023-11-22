9d0067d2c4e64b0367633135ce6e1062c8c0d65a
Simone Fidanza <fsimone2002@proton.me>
2023-11-22 19:06:51 +0100

    feat(plugins:ui): install `heirline`

---
+++ lua/srv/plugins/ui/heirline.lua
@@ -0,0 +1,9 @@
+--# selene: allow(mixed_table)
+
+return {
+  ---Heirline.nvim is a no-nonsense Neovim Statusline plugin designed around recursive
+  ---inheritance to be exceptionally fast and versatile.
+  "rebelot/heirline.nvim",
+  event = "VeryLazy",
+  opts = {},
+}
