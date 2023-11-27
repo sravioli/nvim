6f6a7d1b93af228cbb5c890e153197446fd84c2e
Simone Fidanza <fsimone2002@proton.me>
2023-11-27 18:35:17 +0100

    feat(lsp:servers): enable all type hints for `lua_ls`

--- lua/srv/plugins/lsp/servers/lua_ls.lua
+++ lua/srv/plugins/lsp/servers/lua_ls.lua
@@ -23,13 +23,13 @@ return {
 
   hint = {
     enable = true,
-    arrayIndex = "Disable",
+    arrayIndex = "Enable",
+    setType = true,
   },
 
   workspace = {
     useGitIgnore = false,
     library = {
-      -- vim.api.nvim_get_runtime_file("", true),
       [vim.fn.expand "$VIMRUNTIME/lua"] = true,
       [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
       [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
