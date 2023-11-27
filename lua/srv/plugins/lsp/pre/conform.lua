ae0278d193a1ec24d8427b799cdd46d4c7d4c50e
Simone Fidanza <fsimone2002@proton.me>
2023-11-27 19:38:29 +0100

    feat(lsp:pre): install `conform.nvim`

---
+++ lua/srv/plugins/lsp/pre/conform.lua
@@ -0,0 +1,27 @@
+return {
+	---Lightweight yet powerful formatter plugin for Neovim
+	"stevearc/conform.nvim",
+	event = require("srv.utils.event").events.LazyFile,
+	cmd = "ConformInfo",
+	keys = {
+		{
+			"<leader>cf",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "󰉢  Format buffer",
+		},
+	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+		},
+
+		format_on_save = { timeout_ms = 500, lsp_fallback = true },
+	},
+}
