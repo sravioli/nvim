2673ebe5acfe7a736e4aab7ca7c56ae8f3ad276a
Simone Fidanza <fsimone2002@proton.me>
2023-11-27 18:55:56 +0100

    feat(utils:heirline): add heirline init

---
+++ lua/srv/utils/heirline/init.lua
@@ -0,0 +1,13 @@
+---Heirline statuslines
+---@class HeirlineLines
+---@field statusline table
+local M = {}
+
+setmetatable(M, {
+	__index = function(t, k)
+		t[k] = require("srv.utils.heirline." .. k)
+		return t[k]
+	end,
+})
+
+return M
