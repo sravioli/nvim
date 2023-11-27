17b5b9fac3477956920d8c6ce3e2ace75883e4c8
Simone Fidanza <fsimone2002@proton.me>
2023-11-27 12:09:55 +0100

    feat(plugins:ui): add heirline config, hide statusline until load

--- lua/srv/plugins/ui/heirline.lua
+++ lua/srv/plugins/ui/heirline.lua
@@ -1,9 +1,28 @@
---# selene: allow(mixed_table)
-
 return {
   ---Heirline.nvim is a no-nonsense Neovim Statusline plugin designed around recursive
   ---inheritance to be exceptionally fast and versatile.
   "rebelot/heirline.nvim",
+  dependencies = {
+    { "rebelot/kanagawa.nvim" },
+    { "nvim-tree/nvim-web-devicons" },
+  },
   event = "VeryLazy",
-  opts = {},
+  init = function()
+    vim.g.heirline_lastatus = vim.o.laststatus
+    if vim.fn.argc(-1) > 0 then
+      vim.o.statusline = " " -- set an empty statusline till lualine loads
+    else
+      vim.o.laststatus = 0 -- hide the statusline on the starter page
+    end
+  end,
+  opts = function()
+    if not pcall(require, "heirline") then
+      return
+    end
+
+    vim.o.laststatus = vim.g.heirline_lastatus
+    return {
+      statusline = require "srv.utils.heirline.statusline",
+    }
+  end,
 }
