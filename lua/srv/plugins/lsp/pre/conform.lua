92c860a92aa2134006281b2b371e86bf157d5e34
Simone Fidanza <fsimone2002@proton.me>
2023-11-28 15:40:15 +0100

    fix(lsp:pre): disable conform.nvim

--- lua/srv/plugins/lsp/pre/conform.lua
+++ lua/srv/plugins/lsp/pre/conform.lua
@@ -1,27 +1,28 @@
 return {
-	---Lightweight yet powerful formatter plugin for Neovim
-	"stevearc/conform.nvim",
-	event = require("srv.utils.event").events.LazyFile,
-	cmd = "ConformInfo",
-	keys = {
-		{
-			"<leader>cf",
-			function()
-				require("conform").format({ async = true, lsp_fallback = true })
-			end,
-			mode = "",
-			desc = "󰉢  Format buffer",
-		},
-	},
-	init = function()
-		-- If you want the formatexpr, here is the place to set it
-		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-	end,
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-		},
+  ---Lightweight yet powerful formatter plugin for Neovim
+  "stevearc/conform.nvim",
+  enabled = false,
+  event = require("srv.utils.event").events.LazyFile,
+  cmd = "ConformInfo",
+  keys = {
+    {
+      "<leader>cf",
+      function()
+        require("conform").format { async = true, lsp_fallback = true }
+      end,
+      mode = "",
+      desc = "󰉢  Format buffer",
+    },
+  },
+  init = function()
+    -- If you want the formatexpr, here is the place to set it
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+    },
 
-		format_on_save = { timeout_ms = 500, lsp_fallback = true },
-	},
+    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+  },
 }
