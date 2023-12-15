140f6cbcb6c2fb387a78d23050d8f8c6502851fd
Simone Fidanza <fsimone2002@proton.me>
2023-12-15 16:35:28 +0100

    fix(utils:tree): remove nvim-tree conflicting keymap

--- lua/srv/utils/fun/tree.lua
+++ lua/srv/utils/fun/tree.lua
@@ -46,7 +46,6 @@ M.on_attach = function(bufnr)
   vim.keymap.set("n", "l", edit_or_open, opts "Edit Or Open")
   vim.keymap.set("n", "L", vsplit_preview, opts "Vsplit Preview")
   vim.keymap.set("n", "h", api.node.navigate.parent_close, opts "Close")
-  vim.keymap.set("n", "H", api.tree.collapse_all, opts "Collapse All")
   --~ }}}
 
   ---open on file creation
