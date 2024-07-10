c652a01ad27cbc45c5b0e31e9fc5bddabe899897
sravioli <sravioli+github@proton.me>
2024-07-10 22:35:04 +0200

    feat(plugins:thanks): add thanks command

--- lua/srv/plugins/thanks.lua
+++ lua/srv/plugins/thanks.lua
@@ -1,6 +1,7 @@
 return {
   ---Automatically star all the Neovim plugins you use.
   "jsongerber/thanks.nvim",
+  cmd = { "ThanksAll" },
   opts = {
     star_on_startup = true,
     unstar_on_unistall = true,
