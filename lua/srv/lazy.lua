89380a8ab1c941359dd7c53267214c5740e102dd
sravioli <sravioli+github@proton.me>
2024-07-15 22:46:20 +0200

    feat(lazy): update config

--- lua/srv/lazy.lua
+++ lua/srv/lazy.lua
@@ -1,68 +1,64 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
 local config = {
-	defaults = { lazy = true },
-	install = { colorscheme = { "kanagawa" } },
+  defaults = { lazy = true },
+  install = { colorscheme = { "kanagawa" } },
 
-	ui = {
-		border = require("srv.preferences").border,
-		icons = {
-			ft = "",
-			lazy = "󰂠 ",
-			loaded = "",
-			not_loaded = "",
-		},
-	},
+  ui = {
+    border = require("srv.preferences").border,
+  },
 
-	checker = { enabled = true, notify = false },
-	change_detection = { enabled = false },
+  checker = { enabled = true, notify = false },
+  change_detection = { enabled = false },
 
-	performance = {
-		cache = { enabled = true },
-		rtp = {
-			disabled_plugins = {
-				"2html_plugin",
-				"tohtml",
-				"getscript",
-				"getscriptPlugin",
-				"gzip",
-				"logipat",
-				"netrw",
-				"netrwPlugin",
-				"netrwSettings",
-				"netrwFileHandlers",
-				"matchit",
-				"tar",
-				"tarPlugin",
-				"rrhelper",
-				"spellfile_plugin",
-				"vimball",
-				"vimballPlugin",
-				"zip",
-				"zipPlugin",
-				"tutor",
-				"rplugin",
-				"syntax",
-				"synmenu",
-				"optwin",
-				"compiler",
-				"bugreport",
-				"ftplugin",
-				"editorconfig",
-			},
-		},
-	},
+  performance = {
+    cache = { enabled = true },
+    rtp = {
+      disabled_plugins = {
+        "2html_plugin",
+        "tohtml",
+        "getscript",
+        "getscriptPlugin",
+        "gzip",
+        "logipat",
+        "netrw",
+        "netrwPlugin",
+        "netrwSettings",
+        "netrwFileHandlers",
+        "matchit",
+        "tar",
+        "tarPlugin",
+        "rrhelper",
+        "spellfile_plugin",
+        "vimball",
+        "vimballPlugin",
+        "zip",
+        "zipPlugin",
+        "tutor",
+        "rplugin",
+        "syntax",
+        "synmenu",
+        "optwin",
+        "compiler",
+        "bugreport",
+        "ftplugin",
+        "editorconfig",
+      },
+    },
+  },
+  profiling = { loader = false, require = false },
+  -- profiling = { loader = true, require = true },
 }
 
 require("lazy").setup("srv.plugins", config)
