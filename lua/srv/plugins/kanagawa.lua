0b614a5f8c353b775f9964bced54884e0db57cf7
sravioli <sravioli+github@proton.me>
2024-07-15 22:50:59 +0200

    chore(plugins:kanagawa): remove comments

--- lua/srv/plugins/kanagawa.lua
+++ lua/srv/plugins/kanagawa.lua
@@ -16,27 +16,6 @@ return {
       local theme = colors.theme
 
       return {
-        ---temp until kanagawa gets updated for nvim and treesitter nightly
-        -- ["@string.regexp"] = { link = "@string.regex" },
-        -- ["@variable.parameter"] = { link = "@parameter" },
-        -- ["@exception"] = { link = "@exception" },
-        -- ["@string.special.symbol"] = { link = "@symbol" },
-        -- ["@markup.strong"] = { link = "@text.strong" },
-        -- ["@markup.italic"] = { link = "@text.emphasis" },
-        -- ["@markup.heading"] = { link = "@text.title" },
-        -- ["@markup.raw"] = { link = "@text.literal" },
-        -- ["@markup.quote"] = { link = "@text.quote" },
-        -- ["@markup.math"] = { link = "@text.math" },
-        -- ["@markup.environment"] = { link = "@text.environment" },
-        -- ["@markup.environment.name"] = { link = "@text.environment.name" },
-        -- ["@markup.link.url"] = { link = "Special" },
-        -- ["@markup.link.label"] = { link = "Identifier" },
-        -- ["@comment.note"] = { link = "@text.note" },
-        -- ["@comment.warning"] = { link = "@text.warning" },
-        -- ["@comment.danger"] = { link = "@text.danger" },
-        -- ["@diff.plus"] = { link = "@text.diff.add" },
-        -- ["@diff.minus"] = { link = "@text.diff.delete" },
-
         NormalFloat = { bg = "NONE" },
         FloatBorder = { bg = "NONE" },
         FloatTitle = { bg = "NONE" },
