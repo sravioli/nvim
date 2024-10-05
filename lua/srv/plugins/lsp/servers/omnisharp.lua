06451e349bddd8abfa336a6bf67544e65eb33e85
Simone Fidanza <fsimone2002@proton.me>
2024-10-06 00:30:17 +0200

    chore: update lazy-lock.json

---
+++ lua/srv/plugins/lsp/servers/omnisharp.lua
@@ -0,0 +1,14 @@
+return {
+  cmd = { "/home/sravioli/.local/share/nvim/mason/bin/omnisharp" },
+
+  settings = {
+    FormattingOptions = {
+      EnableEditorConfigSupport = true,
+    },
+    MsBuild = {},
+    RoslynExtensionsOptions = {},
+    Sdk = {
+      IncludePrereleases = false,
+    },
+  },
+}
