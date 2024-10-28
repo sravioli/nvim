0e8b36225f63ea2a6f7d89885ad8608942b00902
Simone Fidanza <fsimone2002@proton.me>
2024-10-28 10:22:01 +0100

    feat(norg): update norg formatting on save

--- ftdetect/norg.lua
+++ ftdetect/norg.lua
@@ -1,5 +1,6 @@
 vim.opt_local.conceallevel = 3
 vim.opt_local.concealcursor = "vc"
+vim.opt_local.fileformat = "dos"
 
 vim.api.nvim_set_hl(0, "@neorg.markup.verbatim", { fg = "#98bb6c", bg = "#2a2a37" })
 
@@ -7,6 +8,9 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   group = vim.api.nvim_create_augroup("Neorg", {}),
   pattern = "*.norg",
   callback = function()
+    vim.cmd [[%s:Get your own SQL Server$::gIe]]
+    vim.cmd [[%s/\r/\r/ge]]
+    vim.cmd [[g/^\_$\n\_^$/d]]
     vim.cmd.Neorg "update-metadata"
     vim.api.nvim_feedkeys("gg=G``zzzv", "n", true)
   end,
