aaefff07ee008d6e800b466572550c59fbd838c6
sravioli <sravioli+github@proton.me>
2024-12-01 16:19:24 +0100

    feat(lsp): move lazydev deps in own section

--- lua/srv/plugins/lsp/init.lua
+++ lua/srv/plugins/lsp/init.lua
@@ -8,17 +8,19 @@ return {
     "folke/lazydev.nvim",
     cond = not vim.g.vscode,
     ft = "lua", -- only load on lua files
+    dependencies = {
+      { "gonstoll/wezterm-types", lazy = true },
+      { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+    },
     opts = {
       library = {
         -- See the configuration section for more details
         -- Load luvit types when the `vim.uv` word is found
         { path = "luvit-meta/library", words = { "vim%.uv" } },
-        { path = "wezterm-types", mods = { "wezterm" } },
+        { path = "wezterm-types", mods = { "wezterm", "wt" } },
       },
     },
   },
-  { "justinsgithub/wezterm-types", lazy = true },
-  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
   -- }}}
 
   -- {{{1 fidget.nvim: extensible UI for Neovim notifications and LSP progress messages.
